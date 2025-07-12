08c943d601094d8a5c1b57ca91815e8c93af16ea
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2025-07-12 07:55:39 +0700

    Create day1.py
    Week 1 | Day 1 | #4

---
+++ Week 1/day1.py
@@ -0,0 +1,36 @@
+# 1
+# print("Rafly Anggara Putra")
+
+# 2
+# name = "Rafly Anggara Putra"
+# age = 20
+# height = 5.6
+# is_married = False
+# print("Name:", name)
+# print("Age:", age)
+# print("Height:", height)
+# print("Is Married?", is_married)
+
+# 2
+# name = input("Siapa Namamu? ")
+# print("Selamat Datang, " + name + "!")
+
+# 3
+# x = 3
+# y = 10
+# print(x + y)
+# print(y - x)
+# print(x * y)
+# print(y / x)
+# print(y // x)
+# print(y % x)
+# print(y ** x)
+
+# 4
+# age = int(input("Berapa umur anda? "))
+# if age >= 18:
+#     print("Kamu sudah dewasa")
+# elif age >= 13:
+#     print("Kamu sudah remaja")
+# else:
+#     print("Kamu masih anak-anak")
