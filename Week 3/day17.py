ed6e6875cb65b9fbe732904382a7cbbc348c4c49
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2025-07-27 08:36:29 +0700

    Updated day17.py
    Clear : Student Report Generator

--- Week 3/day17.py
+++ Week 3/day17.py
@@ -30,3 +30,49 @@
 
 
 # Student Report Generator
+import csv
+
+# Step 1: Read student data and calculate average
+def process_student_data(input_file, output_file):
+    try:
+        with open(input_file, 'r') as infile:
+            reader = csv.DictReader(infile)
+            student_report = []
+            for row in reader:
+                name = row['Name']
+                math = float(row['Math'])
+                science = float(row['Science'])
+                english = float(row['English'])
+                average = round((math + science + english) / 3, 2)
+                status = "Pass" if average >= 60 else "Fail"
+                
+                student_report.append({
+                    'Name': name,
+                    "Math": math,
+                    "Science": science,
+                    "English": english,
+                    "Average": average,
+                    "Status": status
+                })
+                
+            # Step 2: Write proccessed data to a new csv
+            with open(output_file, 'w', newline='') as outfile:
+                fieldnames = ['Name', 'Math', 'Science', 'English', 'Average', 'Status']
+                writer = csv.DictWriter(outfile,fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(student_report)
+            
+            print(f"Student report as generated in {output_file} successfully")
+            
+    except FileNotFoundError:
+        print(f"File {input_file} not found")
+    except KeyError:
+        print("Error: Invalid column names in the input file")
+    except Exception as e:
+        print(f"An error occured: {e}")
+        
+# Step 3: Main Program
+input_file = 'Week 3/sample_data/students.csv'
+output_file = 'Week 3/sample_data/student_report.csv'
+
+process_student_data(input_file,output_file)
