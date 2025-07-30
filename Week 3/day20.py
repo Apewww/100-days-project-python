ef1b7a8aec1259fc60322024ca6e554d6a72c659
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2025-07-30 21:54:28 +0700

    Create day20.py
    Dates & Times

---
+++ Week 3/day20.py
@@ -0,0 +1,22 @@
+# Introduciton
+
+# from datetime import datetime
+
+# current_time = datetime.now()
+# print("Current Date and Time:", current_time)
+
+# event_date = datetime(2025, 12, 25, 19, 0, 0)
+# print("Event Date and Time:", event_date)
+
+# current_time = datetime.now()
+# formated_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+# formated_time = current_time.strftime("%d-%m-%Y %H:%M:%S")
+# print("Formated Time:", formated_time)
+
+# event_date = datetime(2025, 12, 25)
+# current_date = datetime.now()
+# time_difference = event_date - current_date
+# print("Days Remaining:", time_difference.days)
+
+
+
