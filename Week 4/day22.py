3c08290343e5d7f61e6601d9261069aa532b05ab
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2025-08-02 20:11:48 +0700

    Update day22.py
    - What are Classes and Objects
    - Understanding Class Attributes and Methods
    - Constructors (__init__ Method)
    - Working with Multiple Objects
    - Day 22: Project Bank Account Simulator

---
+++ Week 4/day22.py
@@ -0,0 +1,44 @@
+# Introduction
+
+# class Car:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+        
+#     def display_info(self):
+#         print(f"This is a {self.brand} {self.model}.")
+        
+
+# # Create an object
+# my_car = Car("Tesla", "Model 3")
+# my_car.display_info()
+
+# your_car = Car("Honda", "Civic")
+# your_car.display_info()
+
+# class  Dog:
+#     def __init__(self, name, breed):
+#         self.name = name
+#         self.bread = breed
+        
+#     def bark(self):
+#         print(f"{self.name} is barking!")
+        
+# # Create objects
+# dog1 = Dog("Buddy", "Golden Retreiver")
+# dog2 = Dog("Max", "Bulldog")
+
+# dog1.bark()
+# dog2.bark()
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+        
+#     def greet(self):
+#         print(f"Hello my name is {self.name} and I am {self.age} years old.")
+        
+
+# person1 = Person("John", 25)
+# person1.greet()
