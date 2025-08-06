2b28e91f2baae10a08c93a1b6e204e9f4fbb7ec8
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2025-08-06 21:07:46 +0700

    Update day25.py
    Day 25 Project: Animal Sound Simulator

--- Week 4/day25.py
+++ Week 4/day25.py
@@ -36,6 +36,7 @@
 # for shape in shapes:
 #     shape.area()
 
+
 # class Bird:
 #     def make_sound(self):
 #         print("Bird chirps!")
@@ -53,3 +54,78 @@
 
 # animal_sounds(bird)
 # animal_sounds(duck)
+
+
+# Animal Sound Simulator
+
+# Base Class
+class Animal:
+    def make_sound(self):
+        print("The animal makes a sound...")
+
+# Derived Classes
+class Dog(Animal):
+    def make_sound(self):
+        print("The dog barks!")
+        
+class Cat(Animal):
+    def make_sound(self):
+        print("The cat meows!")
+
+class Cow(Animal):
+    def make_sound(self):
+        print("The cow moos!")
+
+class Duck(Animal):
+    def make_sound(self):
+        print("The duck quacks!")
+        
+# Simulator Class
+class AnimalSoundSimulator:
+    def __init__(self):
+        self.animals = []
+        
+    def add_animal(self, animal):
+        if isinstance(animal, Animal):
+            self.animals.append(animal)
+            print(f"{animal.__class__.__name__} added to the simulator")
+        else:
+            print(f"{animal.__class__.__name__} is not an animal")
+            
+    def make_all_sounds(self):
+        if not self.animals:
+            print("No animals in the simulator")
+        else:
+            print("\n----- Animal Sounds -----")
+            for animal in self.animals:
+                animal.make_sound()
+                
+# Main Program
+simulator = AnimalSoundSimulator()
+
+while True:
+    print("\n----- Animal Sound Simulator -----")
+    print("1. Add Dog")
+    print("2. Add Cat")
+    print("3. Add Cow")
+    print("4. Add Duck")
+    print("5. Make All Sounds")
+    print("6. Exit")
+    
+    choice = input("Enter your choice (1-6): ")
+    
+    if choice == '1':
+        simulator.add_animal(Dog())
+    elif choice == '2':
+        simulator.add_animal(Cat())
+    elif choice == '3':
+        simulator.add_animal(Cow())
+    elif choice == '4':
+        simulator.add_animal(Duck())
+    elif choice == '5':
+        simulator.make_all_sounds()
+    elif choice == '6':
+        print("Exiting Program.")
+        break
+    else:
+        print("Invalid choice. Please try again.")
