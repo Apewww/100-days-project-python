c17b8cff0c62ecab7c7a317467a7bd4e168695c6
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2025-08-19 21:39:42 +0700

    Update day36.py
    Day 36 Project: Hello Flask App

--- Week 6/day36.py
+++ Week 6/day36.py
@@ -12,16 +12,31 @@
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
+# Understanding Flask Templates
+
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def  hello():
+#     return render_template('index.html', name='Flask Developer')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# Day 36 Project: Hello Flask App
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def  hello():
-    return render_template('index.html', name='Flask Developer')
+def home():
+    return render_template('index.html')
 
+@app.route('/greet/<name>')
+def greet(name):
+    return render_template('greet.html', name=name)
 if __name__ == '__main__':
     app.run(debug=True)
-
-# Understanding Flask Templates
-# Day 36 Project: Hello Flask App
