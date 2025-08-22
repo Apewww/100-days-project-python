d16fb77fa82d2208ddba49088cdc069560d4725f
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2025-08-22 19:13:41 +0700

    Create day38
    # Introduction to Flask Forms
    # Installing Flask-WTF
    # Creating Forms with Flask-WTF
    # Validating User input

---
+++ Week 6/day38/day38.py
@@ -0,0 +1,31 @@
+# Introduction to Flask Forms
+# Installing Flask-WTF
+# Creating Forms with Flask-WTF
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+app = Flask(__name__)
+app.secret_key = 'apew'
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    message = StringField('Message', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    
+@app.route('/', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        flash(f"Message sent successfully from {form.name.data}!", 'success')
+        return redirect(url_for('contact'))
+    return render_template('contact.html', form=form) 
+
+if '__name__' == '__main__':
+    app.run(debug=True)
+    
+      
+# Validating User input
+# Day 38 Project: Contact Form App
